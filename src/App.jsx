c624821dc7e1c183866d036192b502f36ce51359
fogjoe <1055824818@qq.com>
2024-07-13 21:53:00 +0800

    feat: create redux file

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useContext } from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { store, connect, appContext } from './redux.jsx'
 
-const connect = Component => {
-  const Wrapper = props => {
-    const { state, setState, subscribe } = useContext(appContext)
-
-    const [, update] = useState({})
-
-    useEffect(() => {
-      console.log('connect执行了' + Math.random())
-      subscribe(() => {
-        update({})
-      })
-    }, [])
-
-    const dispatch = action => {
-      setState(reducer(state, action))
-    }
-
-    return <Component {...props} dispatch={dispatch} state={state} />
-  }
-
-  return Wrapper
-}
-
-const store = {
-  state: { user: { name: 'frank', age: 18 } },
-  setState(newState) {
-    store.state = newState
-    // 在每次更新的时候通知所有订阅者
-    store.listeners.forEach(fn => fn(store.state))
-  },
-  listeners: [],
-  subscribe(fn) {
-    store.listeners.push(fn)
-
-    // 在订阅完成之后希望可以删除此调用
-    return () => {
-      const index = store.listeners.indexOf(fn)
-      store.listeners.splice(index, 1)
-    }
-  }
-}
-const appContext = React.createContext(null)
 const App = () => {
   return (
     <appContext.Provider value={store}>
@@ -83,21 +39,6 @@ const User = connect(({ state }) => {
   console.log('User执行了' + Math.random())
   return <div>User:{state.user.name}</div>
 })
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case 'updateUser':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          ...payload
-        }
-      }
-    default:
-      return state
-  }
-}
-
 const UserModifier = connect(({ dispatch, state, children }) => {
   console.log('UserModifyer执行了' + Math.random())
   const onChange = e => {
