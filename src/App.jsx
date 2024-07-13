d837ef70455a78fb526ecb5ee71d0701c498f0e8
fogjoe <1055824818@qq.com>
2024-07-13 21:41:56 +0800

    feat: complete redux first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
+const connect = Component => {
+  const Wrapper = props => {
+    const { state, setState, subscribe } = useContext(appContext)
+
+    const [, update] = useState({})
+
+    useEffect(() => {
+      console.log('connect执行了' + Math.random())
+      subscribe(() => {
+        update({})
+      })
+    }, [])
+
+    const dispatch = action => {
+      setState(reducer(state, action))
+    }
+
+    return <Component {...props} dispatch={dispatch} state={state} />
+  }
+
+  return Wrapper
+}
+
+const store = {
+  state: { user: { name: 'frank', age: 18 } },
+  setState(newState) {
+    store.state = newState
+    // 在每次更新的时候通知所有订阅者
+    store.listeners.forEach(fn => fn(store.state))
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+
+    // 在订阅完成之后希望可以删除此调用
+    return () => {
+      const index = store.listeners.indexOf(fn)
+      store.listeners.splice(index, 1)
+    }
+  }
+}
 const appContext = React.createContext(null)
 const App = () => {
-  const [appState, setAppState] = useState({
-    user: { name: 'frank', age: 18 }
-  })
-  const contextValue = { appState, setAppState }
   return (
-    <appContext.Provider value={contextValue}>
+    <appContext.Provider value={store}>
       <大儿子 />
       <二儿子 />
       <幺儿子 />
@@ -40,11 +79,10 @@ const 幺儿子 = () => {
 
   return <section>幺儿子</section>
 }
-const User = () => {
+const User = connect(({ state }) => {
   console.log('User执行了' + Math.random())
-  const contextValue = useContext(appContext)
-  return <div>User:{contextValue.appState.user.name}</div>
-}
+  return <div>User:{state.user.name}</div>
+})
 const reducer = (state, { type, payload }) => {
   switch (type) {
     case 'updateUser':
@@ -58,20 +96,6 @@ const reducer = (state, { type, payload }) => {
     default:
       return state
   }
-}
-
-const connect = Component => {
-  const Wrapper = props => {
-    const { appState, setAppState } = useContext(appContext)
-
-    const dispatch = action => {
-      setAppState(reducer(appState, action))
-    }
-
-    return <Component {...props} dispatch={dispatch} state={appState} />
-  }
-
-  return Wrapper
 }
 
 const UserModifier = connect(({ dispatch, state, children }) => {
